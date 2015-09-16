7f1bfb8cfb4b6a459e6080aa28303251ca97a029
kawanet <u-suke@kawa.net>
2015-09-17 04:59:18 +0900

    11.quote.js: quote tests

---
+++ test/11.quote.js
@@ -0,0 +1,30 @@
+#!/usr/bin/env mocha -R spec
+
+var assert = require("assert");
+
+var TESTNAME = __filename.replace(/^.*\//, "");
+var SQL = require("../sql");
+
+var SQ = "'";
+var DQ = '"';
+var AC = "`";
+
+describe(TESTNAME + " testing", function() {
+  it(SQ, function() {
+    assert.equal(SQL("?", SQ) + "", SQ + SQ + SQ + SQ);
+    assert.equal(SQL("?", SQ + SQ) + "", SQ + SQ + SQ + SQ + SQ + SQ);
+    assert.equal(SQL("??", SQ) + "", AC + SQ + AC);
+    assert.equal(SQL("??", SQ + SQ) + "", AC + SQ + SQ + AC);
+    assert.equal(SQL.Pg("??", SQ) + "", DQ + SQ + DQ);
+    assert.equal(SQL.Pg("??", SQ + SQ) + "", DQ + SQ + SQ + DQ);
+  });
+
+  it(DQ, function() {
+    assert.equal(SQL("?", DQ) + "", SQ + DQ + SQ);
+    assert.equal(SQL("?", DQ + DQ) + "", SQ + DQ + DQ + SQ);
+    assert.equal(SQL("??", DQ) + "", AC + DQ + AC);
+    assert.equal(SQL("??", DQ + DQ) + "", AC + DQ + DQ + AC);
+    assert.equal(SQL.Pg("??", DQ) + "", DQ + DQ + DQ + DQ);
+    assert.equal(SQL.Pg("??", DQ + DQ) + "", DQ + DQ + DQ + DQ + DQ + DQ);
+  });
+});
